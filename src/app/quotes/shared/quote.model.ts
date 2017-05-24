c8e7cd9bf748ca53a9e1365673f48ecd780feba9
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-05-24 17:08:16 -0300

    Created Quote model and hardcoded some quotes

---
+++ src/app/quotes/shared/quote.model.ts
@@ -0,0 +1,6 @@
+export class Quote {
+  id: number;
+  text: string;
+  author: string;
+  movie: string;
+}
---
+++ src/app/quotes/shared/quotes.database.ts
@@ -0,0 +1,7 @@
+import {Quote} from './quote.model';
+
+export const QUOTES: Quote[] = [
+  { id : 1, text : 'You shall not pass!', author : 'Gandalf', movie : 'The Lord of the Rings: The  Fellowship of the Ring'},
+  { id : 2, text : 'There ain\'t no such thing as a free lunch', author : undefined, movie : undefined},
+  { id : 3, text : 'Many that live deserve death. And some that die deserve life. Can you give it to them? Then do not be too eager to deal out death in judgement. For even the very wise cannot see all ends.', author : 'Gandalf', movie : 'The Lord of the Rings: The  Fellowship of the Ring'}
+];
