8b1aca07d657b4e061865353effd91490d317884
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-05-24 23:39:47 -0300

    Included some movie quotes

--- src/app/quotes/shared/quotes.database.ts
+++ src/app/quotes/shared/quotes.database.ts
@@ -3,5 +3,9 @@ import {Quote} from './quote.model';
 export const QUOTES: Quote[] = [
   { id : 1, text : 'You shall not pass!', author : 'Gandalf', movie : 'The Lord of the Rings: The  Fellowship of the Ring'},
   { id : 2, text : 'There ain\'t no such thing as a free lunch', author : undefined, movie : undefined},
-  { id : 3, text : 'Many that live deserve death. And some that die deserve life. Can you give it to them? Then do not be too eager to deal out death in judgement. For even the very wise cannot see all ends.', author : 'Gandalf', movie : 'The Lord of the Rings: The  Fellowship of the Ring'}
+  { id : 3, text : 'Many that live deserve death. And some that die deserve life. Can you give it to them? Then do not be too eager to deal out death in judgement. For even the very wise cannot see all ends.', author : 'Gandalf', movie : 'The Lord of the Rings: The  Fellowship of the Ring'},
+  { id : 4, text : 'There\'s no certainty, only opportunity.', author : 'V', movie : 'V for Vendetta'},
+  { id : 5, text : 'Say hello to my little friend!', author : 'Tony Montana', movie : 'Scarface'},
+  { id : 6, text : 'I\'ll be back.', author : 'Terminator', movie : 'The Terminator'},
+  { id : 7, text : 'Hasta la vista, baby.', author : 'Terminator', movie : 'Terminator 2: Judgement Day'}
 ];
